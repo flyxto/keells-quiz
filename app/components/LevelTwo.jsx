d1f691d603d6d73bed189aa4b536aff5290bc95a
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2024-11-15 22:07:53 +0700

    update level two answer output animation

--- app/components/LevelTwo.jsx
+++ app/components/LevelTwo.jsx
@@ -75,16 +75,35 @@ export default function LevelTwo({ isActive, levelTwo, showAnswer, isWrong }) {
         <>
           {showAnswer ? (
             <>
-              <div className="flex w-screen px-20 items-center gap-20 -translate-y-20">
-                <div className="bg-white h-1 w-full rounded-full"></div>
-                <h1 className="text-white text-7xl font-semibold w-full text-center whitespace-nowrap shake">
-                  Correct Answer is
-                </h1>
-                <div className="bg-white h-1 w-full rounded-full"></div>
-              </div>
+              <AnimatePresence>
+                <motion.div
+                  initial={{ opacity: 1, display: "flex" }} // Initially, the component won't take up space
+                  animate={{ opacity: 0, display: "none" }} // After animation starts, it will be visible and take space
+                  transition={{ delay: 3 }}
+                  className="flex w-screen px-20 items-center gap-20 absolute">
+                  <div className="bg-white h-1 w-full rounded-full"></div>
+                  <h1 className="text-white text-9xl font-semibold w-full text-center whitespace-nowrap answer-scaleUpDown-animation">
+                    Yes, You are correct
+                  </h1>
+                  <div className="bg-white h-1 w-full rounded-full"></div>
+                </motion.div>
+              </AnimatePresence>
+              <AnimatePresence>
+                <motion.div
+                  initial={{ opacity: 0, display: "none" }} // Initially, the component won't take up space
+                  animate={{ opacity: 1, display: "flex" }} // After animation starts, it will be visible and take space
+                  transition={{ delay: 3 }}
+                  className="flex w-screen px-20 items-center justify-center text-center gap-20 absolute top-52">
+                  <div className="bg-white h-1 w-full rounded-full"></div>
+                  <h1 className="text-white text-8xl font-semibold   whitespace-nowrap">
+                    Answer is
+                  </h1>
+                  <div className="bg-white h-1 w-full rounded-full"></div>
+                </motion.div>
+              </AnimatePresence>
               <div
                 className="bg-white w-[1000px] h-48 rounded-bl-[40px] rounded-tr-[40px] p-2 fade-in"
-                style={{ "--fade-delay": "0.5s" }}>
+                style={{ "--fade-delay": "3.5s" }}>
                 <div className=" border-4 border-green-600 w-full h-full rounded-bl-[38px] rounded-tr-[38px] flex justify-center items-center text-center">
                   <p className="text-6xl font-bold text-green-600">{answer}</p>
                 </div>
